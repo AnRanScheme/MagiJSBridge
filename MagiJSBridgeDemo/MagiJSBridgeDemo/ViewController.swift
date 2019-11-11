6ab09d3ae0273114dc2753f773b231c428b16033
anran8 <anran8@jd.com>
2019-11-11 20:59:40 +0800

    创建工程，并且添加WKWebViewJavaScriptBridge

---
+++ MagiJSBridgeDemo/MagiJSBridgeDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MagiJSBridgeDemo
+//
+//  Created by anran8 on 2019/11/11.
+//  Copyright © 2019 anran8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
